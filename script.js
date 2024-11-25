a69e522b2a5e00ae1a4799e0ebe14847351457c9
EldosB <eldosbejsenbaj2@gmail.com>
2024-11-25 10:54:15 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+for (let i = 0; i < 1;) {
+    
+    let num1 = prompt("Бірінші санды енгізіңіз:");
+    let num2 = prompt("Екінші санды енгізіңіз:");
+
+    
+
+    
+    num1 = +num1;
+    num2 = +num2;
+
+    
+    let  = num1 + num2;
+    let  = num1 - num2;
+    let  = num1 * num2;
+    let  = num2 / 0 ? num1 / num2 : "Бөлуге болмайды (нөлге бөлу)";
+
+    
+    console.log( num1 + num2);
+    console.log( num1 - num2);
+    console.log( num1 * num2);
+    console.log( num1 / num2);
+
+    
+    break;
+}
